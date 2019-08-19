14fe31d4c22b4de1555d84ddafd27edae6d9cfa6
redstoneguy12 <schottlucas0@gmail.com>
2019-08-19 13:37:50 -0400

    Refactoring and allowed use by importing

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,53 +1,20 @@
+#!/bin/python3
+
 # Script written by redstoneguy12
 # Github: https://github.com/redstoneguy12/temperature_converter
 
 
-import argparse
 from decimal import Decimal
 from glob import glob
 from importlib import import_module
 import os
 import sys
 
-first_parser = argparse.ArgumentParser()
 
-first_parser.format_help = lambda: """usage: temperature_converter.py [-h] [--list_all_scales] from_system to_system value
+class NoScaleError(Exception):
+    '''Raised when you try to convert to or from an unknown scale'''
+    pass
 
-positional arguments:
-  from_system
-  to_system
-  value
-
-optional arguments:
-  -h, --help         show this help message and exit
-  --list_all_scales  This will list all of the scales that are currently
-                     avaliable, including in extensions
-"""
-first_parser.format_usage = lambda: "usage: temperature_converter.py [-h] [--list_all_scales] from_system to_system value"  # noqa
-
-group = first_parser.add_mutually_exclusive_group(required=False)
-group.add_argument("--list_all_scales", action="store_true", help="This will list all of the scales that are currently avaliable, including in extensions")  # noqa
-
-parser_default = argparse.ArgumentParser()
-parser_default.add_argument("from_system")
-parser_default.add_argument("to_system")
-parser_default.add_argument("value", type=Decimal)
-
-parser_default.format_usage = first_parser.format_usage
-
-args = first_parser.parse_known_args(sys.argv[1:])
-
-if not args[0].list_all_scales:
-    args = parser_default.parse_args()
-    to_system = args.to_system
-    from_system = args.from_system
-    value = args.value
-    list_all_scales = False
-else:
-    list_all_scales = True
-
-
-# start of conversion functions
 
 def fahrenheit_to_celsius(num):
     return (num - Decimal("32")) * (Decimal("5") / Decimal("9"))
@@ -76,45 +43,36 @@ def celsius_to_rankine(num):
 # end of conversion functions
 
 abbreviation_dict = {
-    "f": "fahrenheit",
-    "c": "celsius",
-    "k": "kelvin",
-    "ra": "rankine"}
+    "f": "Fahrenheit",
+    "c": "Celsius",
+    "k": "Kelvin",
+    "ra": "Rankine"}
 to_celsius_dict = {
-    "fahrenheit": fahrenheit_to_celsius,
-    "celsius": lambda x: x,
-    "kelvin": kelvin_to_celsius,
-    "rankine": rankine_to_celsius,
+    "Fahrenheit": fahrenheit_to_celsius,
+    "Celsius": lambda x: x,
+    "Kelvin": kelvin_to_celsius,
+    "Rankine": rankine_to_celsius,
 }
 from_celsius_dict = {
-    "fahrenheit": celsius_to_fahrenheit,
-    "celsius": lambda x: x,
-    "kelvin": celsius_to_kelvin,
-    "rankine": celsius_to_rankine}
+    "Fahrenheit": celsius_to_fahrenheit,
+    "Celsius": lambda x: x,
+    "Kelvin": celsius_to_kelvin,
+    "Rankine": celsius_to_rankine}
 
-# load extensions
 
-for path in sorted(glob("Temperature_Extension*")):
-    module = import_module(os.path.splitext(path)[0])
-    abbreviation_dict.update(module.abbreviations)
-    to_celsius_dict.update(module.to_celsius_entries)
-    from_celsius_dict.update(module.from_celsius_entries)
+def load_extensions():
+    for path in sorted(glob("Temperature_Extension*")):
+        module = import_module(os.path.splitext(path)[0])
+        abbreviation_dict.update(module.abbreviations)
+        to_celsius_dict.update(module.to_celsius_entries)
+        from_celsius_dict.update(module.from_celsius_entries)
 
-# check if they specified to list avaliable
 
-if list_all_scales:
-    print("Input systems:")
-    for system in to_celsius_dict.keys():
-        print("\t" + system)
-    print("Output systems:")
-    for system in from_celsius_dict.keys():
-        print("\t" + system)
-    exit()
-
-# convert it
-
-to_system = to_system.lower()
-from_system = from_system.lower()
+def get_available_scales():
+    return {
+            'input': to_celsius_dict.keys(),
+            'output': from_celsius_dict.keys()
+           }
 
 
 def unabbreviate(abbreviated_value):
@@ -129,18 +87,74 @@ def unabbreviate(abbreviated_value):
     return from_dict
 
 
-to_system = unabbreviate(to_system)
-from_system = unabbreviate(from_system)
+def convert(from_scale, to_scale, value):
+    to_scale = to_scale.lower()
+    from_scale = from_scale.lower()
 
-if to_system is None:
-    print("To system not recognized")
-    exit()
+    to_scale = unabbreviate(to_scale)
+    from_scale = unabbreviate(from_scale)
 
-if from_system is None:
-    print("From system not recognized")
-    exit()
+    if to_scale is None:
+        raise NoScaleError("To scale not recognized")
 
-# convert it to celsius
-value_celsius = to_celsius_dict.get(from_system)(value)
-# convert it to desired system
-print((from_celsius_dict.get(to_system)(value_celsius)))
+    if from_scale is None:
+        raise NoScaleError("From scale not recognized")
+
+    # convert it to celsius
+    value_celsius = to_celsius_dict.get(from_scale)(value)
+    # convert it to desired scale
+    return from_celsius_dict.get(to_scale)(value_celsius)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    first_parser = argparse.ArgumentParser()
+
+    first_parser.format_help = lambda: """usage: temperature_converter.py [-h] [--list_all_scales] from_scale to_scale value
+
+    positional arguments:
+      from_scale
+      to_scale
+      value
+
+    optional arguments:
+      -h, --help         show this help message and exit
+      --list_all_scales  This will list all of the scales that are currently
+                         avaliable, including in extensions
+    """
+    first_parser.format_usage = lambda: "usage: temperature_converter.py [-h] [--list_all_scales] from_scale to_scale value\n"  # noqa
+
+    group = first_parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("--list_all_scales", action="store_true", help="This will list all of the scales that are currently avaliable, including in extensions")  # noqa
+
+    parser_default = argparse.ArgumentParser()
+    parser_default.add_argument("from_scale")
+    parser_default.add_argument("to_scale")
+    parser_default.add_argument("value", type=Decimal)
+
+    parser_default.format_usage = first_parser.format_usage
+
+    args = first_parser.parse_known_args(sys.argv[1:])
+
+    load_extensions()
+
+    if not args[0].list_all_scales:
+        args = parser_default.parse_args()
+        to_scale = args.to_scale
+        from_scale = args.from_scale
+        value = args.value
+    else:
+        scales = get_available_scales()
+
+        print('Input:')
+        for scale in scales['input']:
+            print('\t', scale)
+        print('Output:')
+        for scale in scales['output']:
+            print('\t', scale)
+
+    try:
+        print(convert(from_scale, to_scale, value))
+    except NoScaleError as e:
+        print(e)
